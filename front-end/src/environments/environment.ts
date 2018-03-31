e1cb78dcfaccb8ebd238ce81f45387218cdda78c
Daghan Acay <daghanacay@gmail.com>
2018-03-31 15:59:47 +1100

    Addin step3 with cognito

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -6,5 +6,17 @@
 export const environment = {
   production: false,
   apiUrl: 'https://aob8p3u5nf.execute-api.ap-southeast-2.amazonaws.com/develop', // i.e.'https://beepboop.execute-api.us-west-2.amazonaws.com/Prod'
-  apiKey: 'PrgEv5W5Nz6YALWrXZD145gsOkji1Dw560AVoMDI'
+  apiKey: 'PrgEv5W5Nz6YALWrXZD145gsOkji1Dw560AVoMDI',
+
+  /************ RESOURCE IDENTIFIERS *************/
+
+  poolData: { 
+      UserPoolId : 'ap-southeast-2_ykwNO2wLR', //CognitoUserPool
+      ClientId : '3l0ee3llpltmnmpcv2qnus84cu', //CognitoUserPoolClient 
+      Paranoia : 7
+  },
+  region: 'ap-southeast-2', //Region Matching CognitoUserPool region
+
+  identityPool: 'ap-southeast-2:e4b6ced3-72ed-4e55-bf5a-af69dae10e54', //CognitoIdentityPool 
+  googleId: '911862631584-ufpneb1emvaieah46s3gk1d7unfb4lb8.apps.googleusercontent.com'
 };
